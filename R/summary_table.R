2508c79893b6f7bf0467d752b9b1fb70b6c5a6f5
Joshua Entrop <entjos@users.noreply.github.com>
2023-02-19 16:05:30 +0100

    Added summary table functions to package

---
+++ R/summary_table.R
@@ -0,0 +1,359 @@
+#' Summary Table
+#'
+#' Creates stratified summary table with rates.
+#'
+#' @param data
+#'    A `data.frame` that includes the data that should be summarised.
+#'
+#' @param vars
+#'    A character vector specifying the variables that should be summarised.
+#'
+#' @param strata
+#'    An optional character vector specifying the variables on which the
+#'    summary table should be stratified on.
+#'
+#' @param overall
+#'    If `TRUE` an additionall summary table will be created including the
+#'    complete dataset. This can be used in combination with `strata`.
+#'
+#' @param get_rates
+#'    If `TRUE` the summary tables includes events rates for events specified
+#'    in the `event` argument for each level of variables in `var`. Rates can
+#'    only be estimated for factor variables in `var`.
+#'
+#' @param  event
+#'    Name of the event indicator that should be used to compute rates.
+#'    Needs to be specified if `get_rates == TRUE`.
+#'
+#' @param st
+#'    Name of the varaible that includes the survival time that should be used
+#'    to comptue rates. Needs to be specified if `get_rates == TRUE`.
+#'
+#' @return
+#'    A `data.frame` with the following columns:
+#'    - `varname`: Including the name of the variable or the name of the category for factor variables
+#'    - `mean`: The mean of the variable, i.e. the proportion for binary variables
+#'    - `p25`: The 25th percentile (only for metric variables.)
+#'    - `p75`: The 75th percentile (only for metric varaibles.)
+#'    - `min`: Minimum of the variable
+#'    - `max`: Maximum of the variable
+#'    - `n_category`: The number of non-NA observations for each variable or cateogry
+#'    - `n_total`: The number of non-NA observations for the variable
+#'    - `n_NA`: The number of NA observations for this variable or category
+#'
+#' @examples
+#' require(rstpm2)
+#' data("brcancer")
+#'
+#' # Simple example
+#' summary_table(brcancer,
+#'               vars = c("x1", "x2", "x3"))
+#'
+#' # Example with stratas
+#' summary_table(brcancer,
+#'               vars = c("x1", "x2", "x3"),
+#'               strata = c("hormon", "x4"))
+#'
+#' # Example with strata and rates
+#' brcancer$x4 <- as.factor(brcancer$x4)
+#'
+#' summary_table(brcancer,
+#'               vars      = c("x1", "x2", "x3", "x4"),
+#'               strata    = c("hormon"),
+#'               get_rates = TRUE,
+#'               event     = "censrec",
+#'               st        = "rectime")
+#'
+#' @importFrom data.table :=
+#' @export summary_table
+
+summary_table <- function(data,
+                          vars,
+                          strata = NULL,
+                          overall = FALSE,
+                          get_rates = FALSE,
+                          event = NULL,
+                          st = NULL){
+
+  # Convert data.table to data.frame ------------------------------------------
+
+  df <- data.table::setDT(data.table::copy(data))
+
+  # Checks --------------------------------------------------------------------
+
+  if(get_rates & is.null(event) & is.null(st)){
+    stop("For competing rates both the `event` and `st`
+         argument need to be specified.")
+  }
+
+  if(!all(vars %in% colnames(df))){
+    stop("Could not find all variables specified in `vars` in `df`.")
+  }
+
+  if(!all(strata %in% colnames(df))){
+    stop("Could not find all variables specified in `strata` in `df`.")
+  }
+
+  if(overall & is.null(strata)){
+    message("Setting overall to TRUE without specifying the strata argument
+            is most likely useless.")
+  }
+
+  if(get_rates){
+
+    if(any(vapply(df[, vars, with = FALSE], is.factor, logical(1)))){
+
+      message("Rates will only be estimted for factor variables in `vars`.")
+
+    } else {
+
+      stop(paste("Rates can only be estimted for factor variables in `vars`.",
+                 "Please make sure that at least one variable specified in",
+                 "`vars` is of class `factor`."))
+
+    }
+
+  }
+
+  # Create stratified summary table -------------------------------------------
+
+  if(!is.null(strata)){
+
+    # Get unique values of strata variables
+    strata_values <- lapply(strata, function(x){unique(df[[x]])})
+
+    # Create matrix with posssible combination of unique strata values
+    stratas <- expand.grid(strata_values)
+
+    # Subset each strat and obtain summary variables
+    out_strat <- lapply(seq_len(nrow(stratas)), function(i){
+
+      concat_stratas <- do.call(paste, df[, strata, with = FALSE])
+
+      if(length(strata) > 1){
+
+        concat_strata <- do.call(paste, stratas[i, ])
+
+        strat_sub <- concat_strata == concat_strata
+
+      } else {
+
+        strat_sub <- df[, get(strata)] == as.character(stratas[i, ])
+
+      }
+
+      strata_df <- subset(df, strat_sub)
+
+      temp_out  <- lapply(vars,
+                          function(x){
+                            var_summary(df  = strata_df,
+                                        var = x)
+                          }) |> data.table::rbindlist()
+
+      if(get_rates){
+
+        temp_out_rates <-
+          lapply(vars[vapply(df[, vars, with = FALSE], is.factor,  logical(1))],
+                 function(x){
+                   summary_rates(df  = strata_df,
+                                 var = x,
+                                 event = event,
+                                 st    = st)
+                 }) |> data.table::rbindlist()
+
+        return(merge(temp_out, temp_out_rates,
+                     by = "varname",
+                     all.x = TRUE))
+
+      } else {
+        return(temp_out)
+      }
+
+    })
+
+    # Create list labels
+    stratas[] <- lapply(stratas, as.character)
+
+    labs <- vapply(seq_len(nrow(stratas)),
+                   function(i){paste(strata,
+                                     stratas[i,],
+                                     sep = "==",
+                                     collapse = "_")
+                   }, FUN.VALUE = character(1))
+
+    # Label list elements
+    names(out_strat) <- labs
+
+  }
+
+  # Create non-stratified summary table ---------------------------------------
+
+  if(is.null(strata) | overall){
+
+    out_overall <- lapply(vars, var_summary, df = df) |>
+      data.table::rbindlist()
+
+    if(get_rates){
+
+      temp_out_rates <-
+        lapply(vars[vapply(df[, vars, with = FALSE], is.factor, logical(1))],
+               function(x){
+                 summary_rates(df  = df,
+                               var = x,
+                               event = event,
+                               st    = st)
+               }) |> data.table::rbindlist()
+
+      out_overall <- merge(out_overall, temp_out_rates,
+                           by = "varname",
+                           all.x = TRUE)
+
+    }
+
+  }
+
+  # Define output object ------------------------------------------------------
+
+  if(is.null(strata)){
+
+    out <- out_overall
+
+  } else if(overall){
+
+    out <- append(out_strat, list(out_overall))
+
+    names(out) <- c(names(out_strat), "overall")
+
+  } else {
+
+    out <- out_strat
+  }
+
+  return(as.data.frame(out))
+
+}
+
+# var_summary function --------------------------------------------------------
+
+var_summary <- function(df, var){
+
+  # Calculate numbers and proportion in each category for factor variables
+  if(class(df[[var]]) == "factor"){
+
+    lev <- levels(df[[var]])
+
+    out <- lapply(seq_along(lev), function(i){
+
+      mean <- mean(as.numeric(df[[var]] == lev[i]),
+                   na.rm = TRUE) |>
+        round(3)
+
+      p25  <-  NA
+
+      p75  <-  NA
+
+      min  <- 0
+
+      max  <- 1
+
+      n_category <- sum(df[[var]] == lev[i], na.rm = TRUE)
+
+      n_total    <- sum(!is.na(df[[var]]))
+
+      n_NA       <- sum(is.na(df[[var]]))
+
+      varname <- paste(var, levels(df[[var]])[i]) |>
+        as.character()
+
+      return(data.frame(varname, mean, p25, p75, min, max,
+                        n_category, n_total, n_NA))
+
+    }) |> data.table::rbindlist()
+
+    # Calculate numbers and proportion in each category for dichotomiesed
+    # integer variables
+  } else if(class(df[[var]]) %in% c("integer", "numeric") &
+            max(df[[var]], na.rm = TRUE) == 1 &
+            min(df[[var]], na.rm = TRUE) == 0){
+
+    mean <- mean(df[[var]],
+                 na.rm = TRUE) |>
+      round(3)
+
+    p25  <- NA
+
+    p75  <- NA
+
+    min  <- 0
+
+    man  <- 1
+
+    n_category <- sum(df[, var], na.rm = TRUE)
+
+    n_total    <- sum(!is.na(df[[var]]))
+
+    n_NA       <- sum(is.na(df[[var]]))
+
+    out <- data.frame(varname = var, mean, p25, p75, min, max,
+                      n_category, n_total, n_NA)
+
+    # Calculate median, q25, q75 for numeric variables
+  } else if(class(df[[var]]) %in% c("integer", "numeric") &
+            max(df[[var]], na.rm = TRUE) != 1){
+
+    mean <- stats::quantile(df[[var]], 0.5 , na.rm = TRUE,
+                            names = FALSE) |>
+      round(1)
+
+    p25  <- stats::quantile(df[[var]], 0.25, na.rm = TRUE,
+                            names = FALSE) |>
+      round(1)
+
+    p75  <- stats::quantile(df[[var]], 0.75, na.rm = TRUE,
+                            names = FALSE) |>
+      round(1)
+
+    min <- min(df[[var]], na.rm = TRUE)
+
+    max <- max(df[[var]], na.rm = TRUE)
+
+    n_category <- NA
+
+    n_total    <- sum(!is.na(df[[var]]))
+
+    n_NA       <- sum(is.na(df[[var]]))
+
+    out <- data.frame(varname = var, mean, p25, p75, min, max,
+                      n_category, n_total, n_NA)
+
+  }
+  return(out)
+}
+
+# summary_rates function ------------------------------------------------------
+
+summary_rates <- function(df, var, event, st){
+
+  no_events <- t_at_risk <- p_events <- NULL # Due to NSE notes in R CMD check
+
+  # Get no. births by variable
+  rate_dt <- data.table::copy(df)
+
+  rate_dt <- rate_dt[!is.na(get(var)),
+                     list(no_events = sum(get(event)),
+                          t_at_risk = sum(get(st))),
+                     by = get(var)]
+
+  rate_dt[, p_events := no_events / sum(no_events)]
+
+  # Calculate rates with CIs
+  rate <- epiR::epi.conf(as.matrix(rate_dt[, c("no_events", "t_at_risk")]),
+                         ctype = "inc.rate",
+                         method = "exact")
+
+  # Improve naming
+  names(rate) <- paste0("rate_", names(rate))
+
+  return(cbind(varname = paste(var, levels(df[[var]])), rate_dt[, -1], rate))
+
+}
